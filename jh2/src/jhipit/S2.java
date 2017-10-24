31bf47ee921f53ccf0dc01b7d03897659b20e41d
mhovm <mhovm@DESKTOP-MKNGVUM>
2017-10-24 08:04:02 +0200

    Iterate over jiras

--- jh2/src/jhipit/S2.java
+++ jh2/src/jhipit/S2.java
@@ -1,5 +1,10 @@
 package jhipit;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -12,11 +17,31 @@ public class S2 {
 		foo.parse();
 	}
 
+	private String getFromFile() {
+		String s;
+		try {
+
+			File f = new File("c:\\jhres\\dgcxjson_cleaned.txt");
+			BufferedReader b = new BufferedReader(new FileReader(f));
+			return b.readLine();
+//			String readLine = "";
+//			while ((readLine = b.readLine()) != null) {
+//				System.out.println(readLine);
+//			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		s = "{\"album_id\":\"7596\",\"album_title\":\"Tjoho\"}";
+		return s;
+	}
+
 	private void parse() {
 		System.out.println("In");
-		String str = "{\"album_id\":\"7596\",\"album_title\":\"Tjoho\"}";
-        Gson gson = new GsonBuilder().create();
-        Albums a = gson.fromJson(str, Albums.class);
-        System.out.println(a);
+		String str = getFromFile();
+		Gson gson = new GsonBuilder().create();
+		JIRAResults j = gson.fromJson(str, JIRAResults.class);
+		System.out.println(j);
 	}
 }
+
